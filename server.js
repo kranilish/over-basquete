947018b4b4b8e8c8c5c22bcd75d4edec2f53f98c
kranilish <kranilish@gmail.com>
2026-06-15 14:33:50 -0300

    Delete server.js

--- server.js
+++
@@ -1,1 +0,0 @@
-const express=require("express");const cors=require("cors");const app=express();app.use(cors());app.use(express.static("public"));const PORT=process.env.PORT||3000;app.listen(PORT,()=>console.log("Servidor rodando em http://localhost:"+PORT));
